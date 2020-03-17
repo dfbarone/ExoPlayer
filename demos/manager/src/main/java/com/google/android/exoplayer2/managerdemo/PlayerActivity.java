bbfa906aaec77c29d79f828efde341a9e682133f
Dominic Barone <dominic.f.barone@gmail.com>
2020-03-17 00:03:27 -0400

    Remove DemoPlayerManager. Just move method overrides inside of initialization of new SimpleExoPlayerManager.

--- demos/manager/src/main/java/com/google/android/exoplayer2/managerdemo/PlayerActivity.java
+++ demos/manager/src/main/java/com/google/android/exoplayer2/managerdemo/PlayerActivity.java
@@ -16,35 +16,172 @@
 package com.google.android.exoplayer2.managerdemo;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 import android.view.KeyEvent;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.dfbarone.android.exoplayer2.manager.PlayerManager;
+import com.dfbarone.android.exoplayer2.manager.Sample;
+import com.dfbarone.android.exoplayer2.manager.SimpleExoPlayerManager;
+import com.dfbarone.android.exoplayer2.manager.util.ContextHelper;
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.RenderersFactory;
+import com.google.android.exoplayer2.drm.DrmSessionManager;
+import com.google.android.exoplayer2.mediacodec.MediaCodecRenderer;
+import com.google.android.exoplayer2.mediacodec.MediaCodecUtil;
+import com.google.android.exoplayer2.offline.DownloadHelper;
+import com.google.android.exoplayer2.offline.DownloadRequest;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.MediaSourceFactory;
+import com.google.android.exoplayer2.source.ads.AdsLoader;
+import com.google.android.exoplayer2.source.ads.AdsMediaSource;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.HttpDataSource;
+import com.google.android.exoplayer2.util.ErrorMessageProvider;
 import com.google.android.exoplayer2.util.Util;
+import java.lang.reflect.Constructor;
 
-/** An activity that plays media using {@link DemoPlayerManager}. */
+/** An activity that plays media using {@link SimpleExoPlayerManager}. */
 public class PlayerActivity extends AppCompatActivity
     implements PlayerManager.EventListener {
 
   private static final String TAG = PlayerActivity.class.getSimpleName();
 
-  private DemoPlayerManager playerManager;
+  private SimpleExoPlayerManager playerManager;
 
   // Activity lifecycle
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
-    String sphericalStereoMode = getIntent().getStringExtra(DemoPlayerManager.SPHERICAL_STEREO_MODE_EXTRA);
+    String sphericalStereoMode = getIntent().getStringExtra(SimpleExoPlayerManager.SPHERICAL_STEREO_MODE_EXTRA);
     if (sphericalStereoMode != null) {
       setTheme(R.style.PlayerTheme_Spherical);
     }
     super.onCreate(savedInstanceState);
     setContentView(R.layout.player_activity);
 
-    playerManager = new DemoPlayerManager(this, findViewById(R.id.root));
+    playerManager = new SimpleExoPlayerManager(this, findViewById(R.id.root)) {
+      @Override
+      public RenderersFactory buildRenderersFactory(boolean preferExtensionRenderer) {
+        return ((DemoApplication) ContextHelper.getApplication(getContext())).buildRenderersFactory(preferExtensionRenderer);
+      }
+
+      @Override
+      public DataSource.Factory buildDataSourceFactory() {
+        return ((DemoApplication) ContextHelper.getApplication(getContext())).buildDataSourceFactory();
+      }
+
+      @Override
+      public HttpDataSource.Factory buildHttpDataSourceFactory() {
+        return ((DemoApplication) ContextHelper.getApplication(getContext())).buildHttpDataSourceFactory();
+      }
+
+      @Override
+      public MediaSource createLeafMediaSource(Sample.UriSample paramters) {
+        DownloadRequest downloadRequest =
+            ((DemoApplication) ContextHelper.getApplication(getContext())).getDownloadTracker().getDownloadRequest(paramters.uri);
+        if (downloadRequest != null) {
+          return DownloadHelper.createMediaSource(downloadRequest, dataSourceFactory);
+        }
+        return super.createLeafMediaSource(paramters);
+      }
+
+      @Override
+      @Nullable
+      public MediaSource createAdsMediaSource(MediaSource mediaSource, Uri adTagUri) {
+        // Load the extension source using reflection so the demo app doesn't have to depend on it.
+        // The ads loader is reused for multiple playbacks, so that ad playback can resume.
+        try {
+          Class<?> loaderClass = Class.forName("com.google.android.exoplayer2.ext.ima.ImaAdsLoader");
+          if (adsLoader == null) {
+            // Full class names used so the LINT.IfChange rule triggers should any of the classes move.
+            // LINT.IfChange
+            Constructor<? extends AdsLoader> loaderConstructor =
+                loaderClass
+                    .asSubclass(AdsLoader.class)
+                    .getConstructor(android.content.Context.class, android.net.Uri.class);
+            // LINT.ThenChange(../../../../../../../../proguard-rules.txt)
+            adsLoader = loaderConstructor.newInstance(this, adTagUri);
+          }
+          final SimpleExoPlayerManager exoPlayerManager = this;
+          MediaSourceFactory adMediaSourceFactory =
+              new MediaSourceFactory() {
+                @Override
+                public MediaSource createMediaSource(Uri uri) {
+                  return exoPlayerManager.createLeafMediaSource(
+                      uri, /* extension=*/ null, DrmSessionManager.getDummyDrmSessionManager());
+                }
+
+                @Override
+                public int[] getSupportedTypes() {
+                  return new int[] {C.TYPE_DASH, C.TYPE_SS, C.TYPE_HLS, C.TYPE_OTHER};
+                }
+              };
+          return new AdsMediaSource(mediaSource, adMediaSourceFactory, adsLoader, playerView);
+        } catch (ClassNotFoundException e) {
+          // IMA extension not loaded.
+          return null;
+        } catch (Exception e) {
+          throw new RuntimeException(e);
+        }
+      }
+
+      @Override
+      public void releaseAdsLoader() {
+        if (adsLoader != null) {
+          adsLoader.release();
+          adsLoader = null;
+          loadedAdTagUri = null;
+          playerView.getOverlayFrameLayout().removeAllViews();
+        }
+      }
+
+      @Override
+      protected ErrorMessageProvider<ExoPlaybackException> getErrorMessageProvider() {
+        return new PlayerErrorMessageProvider();
+      }
+
+      class PlayerErrorMessageProvider implements ErrorMessageProvider<ExoPlaybackException> {
+
+        @Override
+        public Pair<Integer, String> getErrorMessage(ExoPlaybackException e) {
+          String errorString = getString(R.string.error_generic);
+          if (e.type == ExoPlaybackException.TYPE_RENDERER) {
+            Exception cause = e.getRendererException();
+            if (cause instanceof MediaCodecRenderer.DecoderInitializationException) {
+              // Special case for decoder initialization failures.
+              MediaCodecRenderer.DecoderInitializationException decoderInitializationException =
+                  (MediaCodecRenderer.DecoderInitializationException) cause;
+              if (decoderInitializationException.codecInfo == null) {
+                if (decoderInitializationException.getCause() instanceof MediaCodecUtil.DecoderQueryException) {
+                  errorString = getString(R.string.error_querying_decoders);
+                } else if (decoderInitializationException.secureDecoderRequired) {
+                  errorString =
+                      getString(
+                          R.string.error_no_secure_decoder, decoderInitializationException.mimeType);
+                } else {
+                  errorString =
+                      getString(R.string.error_no_decoder, decoderInitializationException.mimeType);
+                }
+              } else {
+                errorString =
+                    getString(
+                        R.string.error_instantiating_decoder,
+                        decoderInitializationException.codecInfo.name);
+              }
+            }
+          }
+          return Pair.create(0, errorString);
+        }
+      }
+    };
+
     playerManager.onRestoreInstanceState(savedInstanceState);
     playerManager.setEventListener(this);
     playerManager.setIntent(getIntent());
@@ -53,8 +190,8 @@ public class PlayerActivity extends AppCompatActivity
 
   @Override
   public void onNewIntent(Intent intent) {
-    playerManager.onNewIntent(intent);
     super.onNewIntent(intent);
+    playerManager.onNewIntent(intent);
   }
 
   @Override
